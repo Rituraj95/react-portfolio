be7cfb8f33d17e19a9a6559170b0e84cf5e71aff
Rituraj Deka <dekarituraj95@gmail.com>
2024-01-11 12:44:38 +0530

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Navbar.css'
+
+const MyNavbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg  ">
+      <a className="navbar-brand" href="#home">Rituraj Deka</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#home">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#about">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default MyNavbar;
